5fe6845f8a4370c4aec7c807efd34fe0d41c733d
Alex Dean <alexander.dean@keplarllp.com>
2013-04-03 10:26:27 +0300

    Fleshing out the collector

--- snowplow.lua
+++ snowplow.lua
@@ -1,11 +1,30 @@
 module("snowplow")
 
 -- Syntax for constants in Lua?
-TRACKER_USER_AGENT = "Lua"
-TRACKER_VERSION = "lua-0.1.0"
-TRACKER_DEFAULT_PLATFORM = "pc"
+local TRACKER_VERSION = "lua-0.1.0"
+local DEFAULT_TRACKER_PLATFORM = "pc"
+local SUPPORTED_TRACKER_PLATFORMS = {"pc", "tv", "mob", "con", "iot"}
 
-ALL_PLATFORMS = {"pc", "tv", "mob", "con", "iot"}
+function set_platform(platform)
+  --[[--
+  The default platform for Lua is "pc". If you are using Lua on
+  another platform (e.g. as part of a console videogame), you
+  can overwrite the platform here.
+
+  @Parameter: platform
+    The short-form name of the platform to set. Can be "pc",
+    "tv", "mob", "con", "iot"
+  --]]--
+end
+
+function set_user_id(user_id)
+  --[[--
+  Sets the business user_id.
+
+  @Parameter; user_id
+    The business user_id to set.
+  --]]--
+end
 
 function track_struct_event(category, action, label, property, value)
   --[[--
@@ -45,12 +64,12 @@ function track_struct_event(category, action, label, property, value)
 
   -- Now let's build the table
   pairs = {
-    {"e", "se"},
-    {"ev_ca", category},
-    {"ev_ac", action},
-    {"ev_la", label},
-    {"ev_pr", property},
-    {"ev_va", value}
+    { "e", "se" },
+    { "ev_ca", category },
+    { "ev_ac", action },
+    { "ev_la", label },
+    { "ev_pr", property },
+    { "ev_va", value }
   } 
 
   -- Finally call track
@@ -75,34 +94,26 @@ function track_struct_event(name, properties)
   end
 end
 
-function set_platform(platform)
-  --[[--
-  The default platform for Lua is "pc". If you are using Lua on
-  another platform (e.g. as part of a console videogame), you
-  can overwrite the platform here.
-
-  @Parameter: platform
-    The short-form name of the platform to set. Can be "pc",
-    "tv", "mob", "con", "iot"
-  --]]--
-end
-
-function set_user_id(user_id)
-  --[[--
-  Sets the business user_id.
-
-  @Parameter; user_id
-    The business user_id to set.
-  --]]--
-end
+-- "Static" module functions below
 
 local function get_transaction_id()
   --[[--
-  Generates a moderately-unique transaction ID - essentially
-  a nonce to make sure this event isn't recorded twice.
+  Generates a moderately-unique six-digit transaction ID
+  - essentially a nonce to make sure this event isn't
+  recorded twice.
   --]]--
 
-  return "TODO"
+  math.randomseed( os.time() )
+  rand = math.random(100000, 999999)
+  return tostring(rand)
+end
+
+local function get_timestamp()
+  --[[--
+  Returns the current timestamp as total milliseconds
+  since epoch.
+  --]]--
+  return (os.time() * 1000)
 end
 
 local function track(event_pairs)
@@ -117,11 +128,12 @@ local function track(event_pairs)
 
   -- Standard pairs
   pairs = {
-    {"tid", get_transaction_id()},
-    {"p",   platform},
-    {"uid", user_id},
-    {"aid", application_id},
-    {"tv",  TRACKER_VERSION}
+    { "tid", get_transaction_id() },
+    { "p",   platform },
+    { "uid", user_id },
+    { "aid", application_id },
+    { "dtm", get_timestamp() },
+    { "tv",  TRACKER_VERSION }
   }
 
   -- Concatenate with event_pairs
