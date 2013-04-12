43b8b12c60a284786efe05e3dae69d15485c4586
Alexander Dean <alex@snowplowanalytics.com>
2013-04-12 19:12:40 +0200

    Update payload.lua

--- lib/payload.lua
+++ lib/payload.lua
@@ -1,4 +1,6 @@
 local escape = require ( "3rdparty.escape" )
+local json = require(  "3rdparty.JSON" )
+local base64 = require( "3rdparty.base64" )
 local http = require( "socket.http" )
 
 local osTime = os.time
