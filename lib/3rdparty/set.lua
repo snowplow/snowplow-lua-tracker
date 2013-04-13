57522a0c1a544e79fc6acfcdc7fefd1bd27b7156
Alexander Dean <alex@snowplowanalytics.com>
2013-04-13 12:51:32 +0300

    Update set.lua

--- lib/3rdparty/set.lua
+++ lib/3rdparty/set.lua
@@ -1,61 +1,44 @@
 module( "set" )
 
 local Set = {}
-local setMeta = { __index = Set }
+Set.mt = { __index = Set }
 
-function newSet (list)
+function newSet (table)
   --[[--
-  Creates a new set from the supplied list of
-  values.
+  Creates a new set from the supplied table.
+  Taken from: http://www.lua.org/pil/13.1.html
 
-  @Parameter: list
-    The list of values for this set
+  @Parameter: table
+    The table containing the value for this set
   --]]--
 
-  -- TODO: update with code from PIL
   local set = {}
-  for _, v in ipairs(list) do set[v] = true end
-  return setmetatable(set, setMeta)
+  setmetatable(set, Set.mt)
+  for _, l in ipairs(t) do set[l] = true end
+  return set
 end
 
 function Set:contains (value)
   --[[--
   Does the set contain this value?
 
-  @Parameter: set
-    Self
   @Parameter: key
     The value to look for in our set
   --]]--
   return self[value]
 end
 
-function Set:mkString (delimiter)
+function Set:tostring ()
   --[[--
-  Scala-esque helper to convert the set
-  into a string, with values separated
-  by the delimiter
-
-  @Parameter: set
-    Self
-  @Parameter: delimiter
-    The string to insert between each value
-    in the set, e.g. ", "
+  Convert a set to a string representation
+  Taken from: http://www.lua.org/pil/13.1.html
   --]]--
 
-  -- TODO: replace with code from PIL
-  if self == {} then return nil end
-  
-  local str = ""
-  local idx = 1
-  local count = size(self) -- TODO: making this syntax up
-  for k, _ in pairs(self) do
-    str = str .. k
-    if idx < count then
-      str = str .. delimiter
-    end
-    idx = idx + 1
+  local s = "{"
+  local sep = ""
+  for e in pairs(self) do
+    s = s .. sep .. e
+    sep = ", "
   end
-
-  return str
+  return s .. "}"
 end
