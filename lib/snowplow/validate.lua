d6b0ab46d53e9a165f69aa26b0d2bc848854b944
Alex Dean <alexander.dean@keplarllp.com>
2013-04-14 03:05:04 +0100

    More tidy up

--- lib/snowplow/validate.lua
+++ lib/snowplow/validate.lua
@@ -10,7 +10,7 @@ local validate = {}
 validate.isBool = function (name, value)
 
   if type(value) ~= "boolean" then
-    error(name .. " is required and must be a boolean")
+    error(name .. " is required and must be a boolean, not [" .. value .. "]")
   end
 end
 
@@ -20,14 +20,14 @@ end
 validate.isNonEmptyTable = function (name, value)
 
   if type(value) ~= "table" or value == {} then
-    error(name .. " is required and must be a non-empty table")
+    error(name .. " is required and must be a non-empty table, not [" .. value .. "]")
   end
 end
 
 validate.isTableOrNil = function (name, value)
 
   if type(value) ~= "table" and value ~= nil then
-    error(name .. " must be a table or nil")
+    error(name .. " must be a table or nil, not [" .. value .. "]")
   end
 end
 
@@ -37,14 +37,14 @@ end
 validate.isNonEmptyString = function (name, value)
 
   if type(value) ~= "string" or value == "" then
-    error(name .. " is required and must be a string")
+    error(name .. " is required and must be a string, not [" .. value .. "]")
   end
 end
 
 validate.isStringOrNil = function (name, value)
 
   if type(value) ~= "string" and value ~= nil then
-    error(name .. " must be a string or nil")
+    error(name .. " must be a string or nil, not [" .. value .. "]")
   end
 end
 
@@ -59,7 +59,7 @@ validate.isStringFromSet = function (set, name, value)
   end
 
   if not set:contains(value) then
-    error(name .. "must be one of " .. set:toString(", ") )
+    error(name .. " must be one of " .. set:toString() .. ", not [" .. value .. "]" )
   end
 end
 
@@ -69,21 +69,21 @@ end
 validate.isNumber = function (name, value)
 
   if type(value) ~= "number" then
-    error(name .. " is required and must be a number")
+    error(name .. " is required and must be a number, not [" .. value .. "]")
   end
 end
 
 validate.isNumberOrNil = function (name, value)
 
   if type(value) ~= "number" and value ~= nil then
-    error(name .. " must be a number or nil")
+    error(name .. " must be a number or nil, not [" .. value .. "]")
   end
 end
 
 validate.isPositiveInt = function (name, value)
 
   if type(value) ~= "number" or value ~= mathAbs(value) or value < 0 then
-    error(name .. "is required and must be a positive integer")
+    error(name .. "is required and must be a positive integer, not [" .. value .. "]")
   end
 end
 
--- t/test.lua
+++ t/test.lua
@@ -3,5 +3,5 @@ package.path = '../lib/?.lua;' .. '../lib/snowplow/?.lua;' .. package.path
 local snowplow = require('snowplow.snowplow')
 
 tracker = snowplow.newTrackerForCf( "dbdbdb" )
-tracker:setPlatform ( "pc2" )
+tracker:setPlatform ( "pc" )
 tracker:trackScreenView( "Game HUD", "23" )
