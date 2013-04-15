3d781c8ee2648d75698946acd72722443c28ea54
Alexander Dean <alex@snowplowanalytics.com>
2013-04-15 13:47:36 +0200

    Update test.lua

--- test_old/test.lua
+++ test_old/test.lua
@@ -2,7 +2,7 @@ package.path = '../lib/?.lua;' .. '../lib/snowplow/?.lua;' .. package.path
 
 local snowplow = require('snowplow.snowplow')
 
-tracker = snowplow.newTrackerForCf( "dbdbdb" )
+tracker = snowplow.newTrackerForCf( "d3rkrsqld9gmqf" )
 tracker:encodeBase64( false )
 tracker:platform ( "pc" )
 tracker:trackScreenView( "Game HUD", "23" )
