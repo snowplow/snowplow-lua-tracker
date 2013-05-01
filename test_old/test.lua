6caff4ed789bd14349d2937b9228825f17a51ecf
Alex Dean <alexander.dean@keplarllp.com>
2013-05-01 10:07:59 +0300

    Testing that two trackers works

--- test_old/test.lua
+++ test_old/test.lua
@@ -6,3 +6,10 @@ tracker = snowplow.newTrackerForCf( "d3rkrsqld9gmqf" )
 tracker:encodeBase64( false )
 tracker:platform ( "pc" )
 tracker:trackScreenView( "Game HUD", "23" )
+
+tracker2 = snowplow.newTrackerForCf( "d3rkrsqld9gmqf" )
+tracker2:encodeBase64( false )
+tracker2:platform ( "iot" )
+tracker2:trackScreenView( "Test", "23" )
+
+tracker:trackScreenView( "Game HUD", "23" )
