f795f88c5d14068239e067404174567b53097007
Greg Leonard <45019882+greg-el@users.noreply.github.com>
2022-03-29 19:01:16 +0100

    Extend micro implementation (close #38)
    This is a small update to the micro functionality, to reduce the scope
     of the coming PR which will add POST/JSON schema support.
    
    - add JSON library `lunajson` to parse micro filters/output
    - rename `micro_url.lua` -> `micro.lua`
    - add functions to get good events and clear micro cache
    - add tests
    - replace `MICRO_URL` with `micro.get_url()` in bad1_spec and
    good1_spec

--- snowplowtracker-scm-1.rockspec
+++ snowplowtracker-scm-1.rockspec
@@ -18,7 +18,8 @@ dependencies = {
   "lua >= 5.1",
   "lua-curl >= 0.3.13-1",
   "base64 >= 1.5-2",
-  "lua_uuid >= 0.2.0-2"
+  "lua_uuid >= 0.2.0-2",
+  "lunajson >= 1.2.3-1"
 }
 build = {
   type = "builtin",
---
+++ spec/micro/micro.lua
@@ -0,0 +1,65 @@
+--- micro.lua
+--
+-- Copyright (c) 2022 Snowplow Analytics Ltd. All rights reserved.
+--
+-- This program is licensed to you under the Apache License Version 2.0,
+-- and you may not use this file except in compliance with the Apache License Version 2.0.
+-- You may obtain a copy of the Apache License Version 2.0 at http://www.apache.org/licenses/LICENSE-2.0.
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the Apache License Version 2.0 is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the Apache License Version 2.0 for the specific language governing permissions and limitations there under.
+--
+-- Authors:     Greg Leonard
+-- Copyright:   Copyright (c) 2022 Snowplow Analytics Ltd
+-- License:     Apache License Version 2.0
+
+local curl = require("cURL")
+local json = require("lunajson")
+
+-- Get the URL for micro
+-- @return string: The $MICRO_URL env var, or default if not set
+local function get_url()
+  return os.getenv("MICRO_URL") or "http://localhost:9090"
+end
+
+-- Clear micro's cache
+local function clear_cache()
+  local c = curl.easy({
+    url = get_url() .. "/micro/reset",
+  })
+  -- We don't want to print the response body
+  c:setopt_writefunction(function() end)
+
+  c:perform()
+end
+
+-- Get good events from micro
+-- @param filter_table table: A optional table of filters to apply to the events
+-- @return table: A table of events
+local function get_good_events(filter_table)
+  local url = get_url() .. "/micro/good"
+  local resp = {}
+  local c = curl.easy({
+    url = url,
+  }):setopt_writefunction(table.insert, resp)
+
+  if filter_table then
+    c:setopt_post(true)
+    c:setopt_httpheader({
+      "Content-Type: application/json",
+    })
+    c:setopt_postfields(json.encode(filter_table))
+  end
+
+  c:perform()
+  -- Data gets written in chunks to resp, so we need to concat it
+  return json.decode(table.concat(resp))
+end
+
+return {
+  get_url = get_url,
+  clear_cache = clear_cache,
+  get_good_events = get_good_events,
+}
